c3355689af60a67562187f2583d53654768265c5
ikbal-e <60185548+ikbal-e@users.noreply.github.com>
2021-07-27 10:43:25 +0300

    added tests

--- Counter-with-Akita/src/app/app.component.spec.ts
+++ Counter-with-Akita/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'Counter-with-Akita'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Counter-with-Akita');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Counter-with-Akita app is running!');
-  });
 });
--- Counter-with-Akita/src/app/counter/counter.component.spec.ts
+++ Counter-with-Akita/src/app/counter/counter.component.spec.ts
@@ -1,16 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CounterComponent } from './counter.component';
+import { CounterService } from './counter.service';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
+  let counterService: CounterService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CounterComponent ]
+      declarations: [ CounterComponent ],
     })
     .compileComponents();
+
+    counterService = TestBed.inject(CounterService);
+  
   });
 
   beforeEach(() => {
@@ -19,7 +24,24 @@ describe('CounterComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('count should be 1 after increase', () => {
     expect(component).toBeTruthy();
+    component.increase();
+    fixture.detectChanges();
+    component.counter$.subscribe(c => expect(c.count).toBe(1));
+  });
+
+  it('count should be -1 after decrease', () => {
+    expect(component).toBeTruthy();
+    component.decrease();
+    fixture.detectChanges();
+    component.counter$.subscribe(c => expect(c.count).toBe(-1));
+  });
+
+  it('count should be 10 after increase by 10', () => {
+    expect(component).toBeTruthy();
+    counterService.increaseBy(10);
+    fixture.detectChanges();
+    component.counter$.subscribe(c => expect(c.count).toBe(10));
   });
 });

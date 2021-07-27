b3e4d9306a892b992b3b8bba103490b9098c0559
ikbal-e <60185548+ikbal-e@users.noreply.github.com>
2021-07-27 10:45:24 +0300

    added tests

--- Counter-with-RxJS/src/app/app.component.spec.ts
+++ Counter-with-RxJS/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Counter-with-RxJS');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Counter-with-RxJS app is running!');
-  });
 });
